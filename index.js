8917a97201f74aafe26d7c955568bc81f04e3c42
Ciaran Murphy <murphyspiderpig@gmail.com>
2022-01-27 15:03:31 +0000

    message buffer and anonymous functions

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const config = require('./config.json')
+const config = require('./config.json');
 
 const client = new Discord.Client(
     { intents: ["GUILD_MESSAGES", "GUILD_MEMBERS", "GUILDS"] }
 );
 
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// client.commands = new Discord.Collection();
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
     console.log('Have you seen the movie Hackers?');
@@ -18,23 +18,16 @@ client.login(config.TOKEN);
 let messageBuffer = new Array(10);
 
 const commands = {
-    'ping':'pong',
-    'eep':'oop'//,
-    // 'echo':messageBuffer[9]
+    'ping': (messages) => 'pong',
+    'eep': (messages) => 'oop',
+    'echo': (messages) => messages[messages.length-1]
 }
-
-// for (const file of commandFiles) {
-//     const command = require(`./commands/${file}`);
-//     // Set a new item in the Collection
-//     // With the key as the command name and the value as the exported module
-//     client.commands.set(command.data.name, command);
-// }
 
 client.on('messageCreate', async message => {
     if (message.author.bot) { return; }
     if (message.content.startsWith(config.PREFIX)) {
         let args = message.content.slice(config.PREFIX.length).toLowerCase().split(" ");
-        message.reply(client.commands[args[0]]);
+        message.reply(commands[args[0]](messageBuffer));
     } else {
         messageBuffer.push(message.content);
         messageBuffer.shift();
